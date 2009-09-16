0393f26f3a45933fedd3c0fdaf734b78236ea932
Thomas Ritz <thomas@galaxy-ritz.de>
2009-09-16 17:07:27 +0200

    name zsh snippets with extension .zsh

---
+++ dot/zsh.d/S99_local.zsh
@@ -0,0 +1,14 @@
+alias t="ssh tux"
+alias p="ssh private"
+
+
+set_git_identity() {
+  if [[ $PWD == */(private|Pictures|Volumes/DOCS)(|/*) ]]; then
+    export GIT_AUTHOR_EMAIL="thomas@galaxy-ritz.de"
+  else
+    unset GIT_AUTHOR_EMAIL
+  fi
+}
+
+typeset -ga chpwd_functions
+chpwd_functions+='set_git_identity'
