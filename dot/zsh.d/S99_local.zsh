9e4581ab8cfa3d21c1d0b3cb5202018e46654ed1
Thomas Ritz <thomas@galaxy-ritz.de>
2009-10-21 20:32:51 +0200

    Use private git identity in directory 'other'

--- dot/zsh.d/S99_local.zsh
+++ dot/zsh.d/S99_local.zsh
@@ -3,7 +3,7 @@ alias p="ssh private"
 
 
 set_git_identity() {
-  if [[ $PWD == */(private|Pictures|Volumes/DOCS)(|/*) ]]; then
+  if [[ $PWD == */(private|other|Pictures|Volumes/DOCS)(|/*) ]]; then
     export GIT_AUTHOR_EMAIL="thomas@galaxy-ritz.de"
   else
     unset GIT_AUTHOR_EMAIL
