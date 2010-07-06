1eaaaa78c282d54532e532920a4ec2d5cc0d22bf
Thomas Ritz <thomas@galaxy-ritz.de>
2010-07-06 14:58:39 +0200

    make prompt compatible with linux terminal

--- dot/zsh.d/S60_git_prompt.zsh
+++ dot/zsh.d/S60_git_prompt.zsh
@@ -8,7 +8,7 @@ function git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   branch=${ref#refs/heads/}
 
-  echo -n " ‹%{$fg[blue]%}${branch}%{$reset_color%}›"
+  echo -n " <%{$fg[blue]%}${branch}%{$reset_color%}>"
   echo
 }
 
@@ -29,7 +29,7 @@ PROMPT+="%{$fg[cyan]%}%(2~.%~.%/)%{$reset_color%}"
 PROMPT+="\$(git_prompt_info)"
 PROMPT+="
 "
-PROMPT+="%{$fg[yellow]%}%(!.#.∴)%{$reset_color%} "
+PROMPT+="%{$fg[yellow]%}%(!.#.%%)%{$reset_color%} "
 
 
 RPROMPT=
@@ -40,25 +40,3 @@ RPROMPT+="%{$fg[cyan]%}%n@%m%{$reset_color%}"
 RPROMPT+="%(?.. %{$fg[red]%}exited %1v%{$reset_color%})"
 
 precmd_functions+='prompt_precmd'
-
-
-# screen sets title to executed command
-# requires in .screenrc:
-#   aclchg :window: -rwx  #?
-#   aclchg :window: +x title
-# http://matthew.loar.name/blog/2007/11/06/zsh__screen_for_great_justice/
-
-function zsh_default_title_precmd {
-  echo -ne "\033]83;title zsh\007"
-}
-
-precmd_functions+='zsh_default_title_precmd'
-
-
-function zsh_title_preexec {
-  local cmdline="$2 "
-  local cmd=${${=cmdline}[1]}
-  echo -ne "\033]83;title $cmd\007"
-}
-
-preexec_functions+='zsh_title_preexec'
